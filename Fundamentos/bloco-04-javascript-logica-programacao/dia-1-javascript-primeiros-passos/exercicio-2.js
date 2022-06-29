691745a1794c58cbd33b86f9b43219b5295ab7a6
FelipeBGFerraz <felipebgferraz@gmail.com>
2022-06-29 18:25:34 -0300

    logica de programação inicio

---
+++ Fundamentos/bloco-04-javascript-logica-programacao/dia-1-javascript-primeiros-passos/exercicio-2.js
@@ -0,0 +1,17 @@
+// const a = 20;
+// const b = 30;
+// if (a > b){
+//     console.log(a);
+// } else{
+//     console.log(b);
+// }
+
+function comparacao(a, b){
+    if (a > b){
+        return a;
+    } else{
+        return b;
+    }
+}
+let comp = comparacao(20, 30)
+console.log(comp);
